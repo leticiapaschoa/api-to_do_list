91088d374c7e061f467fa1c37f6161a237a498e4
Leticia <leticia.paschoa.silva@gmail.com>
2019-06-25 21:31:54 -0300

    Configuração de rotas

---
+++ app.js
@@ -0,0 +1,5 @@
+const express = require('express')
+const app = express()
+ 
+app.use('/', require('./routes/index'));
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,4 @@
-//Criar um novo servidor manualmente (não utilizado)
-//var http = require('http');
-// http.createServer(function(req, res){
-//     res.end('Hello World');
-// }).listen(3000);
+const app = require('./app');
+const port = 3000;
 
-//Criar um servidor usando o pacote Express
-const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+app.listen(port);
