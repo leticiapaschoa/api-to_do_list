ef139e5ad20e035c73363630517bc571b1617dc8
Leticia <leticia.paschoa.silva@gmail.com>
2019-06-28 20:50:30 -0300

    API com MongoDb

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,19 +1,26 @@
-const get = function(req, res){
-    const tasks = [
-        {
-            "title": "Aprender HTML",
-            "description": "Estudar a tag <body>"            
-        },
-        {
-            "title": "Aprender CSS",
-            "description": "Estudar divs"
-        }
-    ];
+const Task = require('../models/Task');
 
-    res.send(tasks);
+const get = async function(req, res){
+    try {
+        const tasks = await Task.find(); //Busca os dados no banco
+        return res.send(tasks);
+    }
+    catch(error )
+    {
+        return res.status(500).send('Erro ao buscar tarefas');
+    }
 }
 
-const create = function(req, res){
+const create = async function(req, res){
+    try{
+
+        const task = await Task.create(req.body);
+        return res.send(task);
+    }
+    catch (error) 
+    {
+        return res.status(500).send('Erro ao cadastrar tarefa');
+    }
 
 }
 
